8dd9cfbd672d96e57d70f9b4ba4df9a953eb4ba4
senorbeast <hrishikesh99@hotmail.com>
2020-12-18 18:14:47 +0530

    initial commit

---
+++ graphql/typeDefs.js
@@ -0,0 +1,17 @@
+const { gql } = require("apollo-server");
+
+module.exports = gql`
+    type User {
+        id: ID!
+        email: String!
+        token: String!
+        username: String!
+        createdAt: String!
+    }
+    input RegisterInput {
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
+`;
